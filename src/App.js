68b1e9c052bda2f7aeb8145c3552ec06f5926ec6
Shaikh <shaikhj2001@gmail.com>
2021-11-04 17:18:02 -0930

    first  Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Componants/Header';
+import Home from './Componants/Home';
+
+
+
+function App() {
+  return (
+    <div className="App">
+   <Header />
+  <Home />
+  
+    </div>
+  );
+}
+
+export default App;
